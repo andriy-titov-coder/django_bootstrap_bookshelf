105baa42f20390f9b960431ac11a63a6e6074133
andriy-titov-coder <andreytitov19880811@gmail.com>
2026-05-27 14:48:46 +0300

    feat: add URL routes for book list, detail and add views

---
+++ src/app_books/urls.py
@@ -0,0 +1,11 @@
+# app_books/urls.py
+from django.urls import path
+from . import views
+
+app_name = 'books'
+
+urlpatterns = [
+    path('', views.book_list, name='book_list'),
+    path('add/', views.book_add, name='book_add'),
+    path('<int:pk>/', views.book_detail, name='book_detail'),
+]
