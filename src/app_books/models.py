b1ec87197ce955fa3b63127ad86ff7cd2ccf3068
andriy-titov-coder <andreytitov19880811@gmail.com>
2026-05-27 14:46:21 +0300

    feat: add Book model and initial migration

--- src/app_books/models.py
+++ src/app_books/models.py
@@ -1,3 +1,20 @@
+# app_books/models.py
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    """Model representing a book in the library."""
+
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+    year = models.IntegerField()
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return a string representation of the book."""
+        return f"{self.title} ({self.author})"
+
+    class Meta:
+        ordering = ['-year']
